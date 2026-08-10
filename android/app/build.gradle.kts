36d9595746b93fd124f5e15b19aef09fb9f2522a
Freddy Mac <freddycalderon1990@gmail.com>
2026-08-10 15:52:28 -0500

    correccion de java

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,20 +30,21 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(
+                org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+            )
+        }
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "ecuador.policianacional.dntic.siipnemovil2"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
@@ -59,15 +60,12 @@ android {
         }
     }
 
-
     buildTypes {
         release {
-
             signingConfig = signingConfigs.getByName("release")
         }
     }
 }
-
 flutter {
     source = "../.."
 }
---
+++ android/build/macos/SourcePackages/firebase_messaging-16.2.0/macos/firebase_messaging/Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// Copyright 2024, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import PackageDescription
+
+let library_version = "16.2.0"
+let firebase_sdk_version: Version = "12.12.0"
+
+let package = Package(
+  name: "firebase_messaging",
+  platforms: [
+    .macOS("10.15"),
+  ],
+  products: [
+    .library(name: "firebase-messaging", targets: ["firebase_messaging"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/firebase/firebase-ios-sdk", from: firebase_sdk_version),
+    .package(name: "firebase_core", path: "../firebase_core-4.7.0"),
+  ],
+  targets: [
+    .target(
+      name: "firebase_messaging",
+      dependencies: [
+        .product(name: "FirebaseMessaging", package: "firebase-ios-sdk"),
+        .product(name: "firebase-core", package: "firebase_core"),
+      ],
+      resources: [
+        .process("Resources"),
+      ],
+      cSettings: [
+        .headerSearchPath("include"),
+        .define("LIBRARY_VERSION", to: "\"\(library_version)\""),
+        .define("LIBRARY_NAME", to: "\"flutter-fire-fcm\""),
+      ]
+    ),
+  ]
+)
